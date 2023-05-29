ed02bbb1e7fa0b627fbda3bc2e38ecccc63a5093
Swarneshwar Senthilkumar <swarnesh9421@gmail.com>
2023-05-29 23:28:59 +0530

    created new customer model in be

--- server/crmapp/models.py
+++ server/crmapp/models.py
@@ -11,3 +11,21 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.first_name
+
+class CustomerDetails(models.Model):
+    customer_id=models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name= models.CharField(max_length=50)
+    contact = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    address1 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    pincode = models.CharField(max_length=10)
+    purchase_date = models.DateField()
+    model = models.CharField(max_length = 30)
+    sr_number = models.CharField(max_length= 50)
+
+    def __str__(self):
+        return self.customer_id
