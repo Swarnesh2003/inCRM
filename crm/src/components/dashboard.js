6837ac12be27ec9dff5c382da71b594871ed235c
Swarneshwar Senthilkumar <swarnesh9421@gmail.com>
2023-05-31 13:01:03 +0530

    front end designs for dashboard

--- crm/src/components/dashboard.js
+++ crm/src/components/dashboard.js
@@ -3,17 +3,43 @@ import {Link, Outlet} from "react-router-dom";
 export default function Dashboard(){
 
     return(
-    <div className='dashboard'>
-        <div className='dashboard-links'>
-            <Link to="/dashboard/addCustomer"><button>Add Customer</button></Link>
-        </div>
-        <div className='dashboard-outlets'>
-            <div className='dashboard-outlet-header'>
-                <h3>Welcome Back!!</h3>
+        <div class="admin">
+        <header class="admin__header">
+          <a href="#" class="logo">
+            <img src=".\image\logo-bg.png" alt="Logo"  className="d-inline-block align-text-top" />
+          </a>
+          <div class="toolbar">
+            <h2>Welcome Back!</h2>
+            <a href="#" class="logout">
+              Log Out
+            </a>
+          </div>
+        </header>
+        <div class='main1'>
+        <nav class="admin__nav">
+          <div class="menu">
+            <div class="menu__item">
+              <a class="menu__link" href="#"><Link to="/dashboard/addCustomer">Add Customer</Link></a>
             </div>
-            <div className='dashboard-outlet-main'>
-                <Outlet />
+            <div class="menu__item">
+              <a class="menu__link" href="#"><Link to="/dashboard/addCustomer">Campaign</Link></a>
             </div>
+            <div class="menu__item">
+              <a class="menu__link" href="#"><Link to="/dashboard/addCustomer">Settings</Link></a>
+            </div>
+            <div class="menu__item">
+              <a class="menu__link" href="#"><Link to="/dashboard/addCustomer">Add Services</Link></a>
+            </div>
+            
+          </div>
+        </nav>
+        <div className='main2'>
+        <main class="admin__main">
+          <Outlet />
+        </main>
         </div>
-    </div>)
+        </div>
+      </div>
+      
+      )
 }
